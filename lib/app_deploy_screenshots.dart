3a93fd018c1c0058d1558d831eddedf58c0c6f3b
Jonah Walker <supposedlysam@gmail.com>
2025-04-06 01:10:18 -0400

    feat: add appDeployScreenshot
    Allows you to provide your own devices

--- lib/app_deploy_screenshots.dart
+++ lib/app_deploy_screenshots.dart
@@ -14,6 +14,54 @@ export 'package:app_deploy_screenshots/golden_toolkit/golden_toolkit.dart';
 ///Sometimes, you want to do a golden test for different stages of animations, so its crucial to have a precise control over pumps and durations
 typedef CustomPump = Future<void> Function(WidgetTester);
 
+/// Function definition for allowing for device or test setup to occur for each device configuration under test
+typedef DeviceSetup = Future<void> Function(Device device, WidgetTester tester);
+
+/// This [appDeployScreenshot] will run [scenarios] for given [devices] list
+///
+/// Will output a single  golden file for each device in [devices] and will append device name to png file
+///
+/// [name] is a file name output, must NOT include extension like .png
+///
+/// [finder] optional finder, defaults to [WidgetsApp]
+///
+/// [customPump] optional pump function, see [CustomPump] documentation
+///
+/// [deviceSetup] allows custom setup after the window changes size.
+/// Takes two pumps to modify the device size. It could take more if the widget tree uses widgets that schedule builds for the next run loop
+/// e.g. StreamBuilder, FutureBuilder
+///
+/// [devices] list of devices to run the tests
+Future<void> appDeployScreenshot(
+  WidgetTester tester,
+  String name, {
+  Finder? finder,
+  CustomPump? customPump,
+  DeviceSetup? deviceSetup,
+  List<Device>? devices,
+}) async {
+  assert(devices == null || devices.isNotEmpty);
+  final deviceSetupPump = deviceSetup ?? _twoPumps;
+  final defaultDevices = AppDeployToolkit.configuration.defaultDevices;
+
+  for (final device in devices ?? defaultDevices) {
+    await tester.binding.runWithDeviceOverrides(
+      device,
+      body: () async {
+        await deviceSetupPump(device, tester);
+        await captureScreenshot(
+          tester,
+          name,
+          customPump: customPump,
+          finder: finder,
+          device: device,
+          fileNameFactory: AppDeployToolkit.configuration.deviceFileNameFactory,
+        );
+      },
+    );
+  }
+}
+
 /// Creates all ios and android screnshots based on app store guidelines
 ///
 /// iPhone: Adding accurate screenshots of your app on the newest devices can help you represent the app's user experience. Keep in mind that we'll use these screenshots for all display sizes and localizations. Screenshots are only required for iOS apps, and only the first 3 will be used on the app installation sheets.
