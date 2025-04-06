1507221c8b6193eca840d4f9f8f029f67544aba4
Jonah Walker <supposedlysam@gmail.com>
2025-04-06 00:17:55 -0400

    test: add counter app with tap

--- test/app_deploy_screenshots_test.dart
+++ test/app_deploy_screenshots_test.dart
@@ -1,19 +1,68 @@
-import 'package:app_deploy_screenshots/golden_toolkit/golden_toolkit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:app_deploy_screenshots/app_deploy_screenshots.dart';
 
 void main() {
   testWidgets('First page screenshot', (tester) async {
     await tester.waitForAssets();
 
-    await tester.pumpWidgetBuilder(const Scaffold(
-      body: Center(child: Text('First page')),
-    ));
+    await tester.pumpWidgetBuilder(const CounterApp());
 
     await tester.pumpAndSettle();
 
-    await createAllByPlatformAndDevice(tester, 'first_page');
+    await captureByPlatformAndDevice(tester, 'initial_state');
+
+    await tester.tap(find.byType(ElevatedButton));
+
+    await tester.pumpAndSettle();
+
+    await captureByPlatformAndDevice(tester, 'incremented_state');
 
     expect(true, isTrue);
   });
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CounterPage());
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_counter'),
+            ElevatedButton(
+              onPressed: _increment,
+              child: const Text('Increment'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
