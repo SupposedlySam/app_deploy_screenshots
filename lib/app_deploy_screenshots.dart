adebda1d677a26b30b14525da4b6364a69600bd9
Jonah Walker <supposedlysam@gmail.com>
2025-04-05 20:49:09 -0400

    chore: export functions and classes

--- lib/app_deploy_screenshots.dart
+++ lib/app_deploy_screenshots.dart
@@ -1,1 +1,3 @@
-
+export 'golden_toolkit/src/app_deploy_golden.dart'
+    show createAllByPlatformAndDevice, appDeployScreenshot;
+export 'golden_toolkit/src/device.dart' show Device, DevicePlatform;
